651d44ff76cb0b3f88a816f26b4ea08df7d179ca
MrIncognitoAKAnitro <contact.itzscratchy@gmail.com>
2024-07-24 15:58:15 +0100

    Update scriptlite.js

--- scriptlite.js
+++ scriptlite.js
@@ -1,18 +1,38 @@
-function openGameModal(gameName) {
-    const modal = document.getElementById('gameModal');
-    document.getElementById('gameTitle').innerText = gameName;
-    document.getElementById('gameFrame').src = ''; // Set URL later
+// Create modals for each game
+for (let i = 1; i <= 10; i++) {
+    let modalHTML = `
+        <div id="gameModal${i}" class="modal">
+            <div class="modal-content">
+                <span class="close-button" onclick="closeGameModal(${i})">&times;</span>
+                <h2 id="gameTitle${i}">Game ${i}</h2>
+                <iframe id="gameFrame${i}" src="" width="100%" height="400px" frameborder="0" allowfullscreen></iframe>
+                <button id="playButton${i}" class="play-button" onclick="togglePlay(${i})">Start Game</button>
+                <button class="fullscreen-button" onclick="toggleFullscreen(${i})">
+                    <svg class="fullscreen-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                        <path d="M32 32C14.3 32 0 46.3 0 64l0 96c0 17.7 14.3 32 32 32s32-14.3 32-32l0-64 64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L32 32zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 96c0 17.7 14.3 32 32 32l96 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0 0-64zM320 32c-17.7 0-32 14.3-32 32s14.3 32 32 32l64 0 0 64c0 17.7 14.3 32 32 32s32-14.3 32-32l0-96c0-17.7-14.3-32-32-32l-96 0zM448 352c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64-64 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l96 0c17.7 0 32-14.3 32-32l0-96z"/>
+                    </svg>
+                </button>
+            </div>
+        </div>
+    `;
+    document.getElementById('gameModals').innerHTML += modalHTML;
+}
+
+function openGameModal(gameName, modalId) {
+    const modal = document.getElementById(`gameModal${modalId}`);
+    document.getElementById(`gameTitle${modalId}`).innerText = gameName;
+    document.getElementById(`gameFrame${modalId}`).src = ''; // Set URL later
     modal.style.display = 'block';
 }
 
-function closeGameModal() {
-    const modal = document.getElementById('gameModal');
+function closeGameModal(modalId) {
+    const modal = document.getElementById(`gameModal${modalId}`);
     modal.style.display = 'none';
 }
 
-document.getElementById('playButton').addEventListener('click', function() {
-    const iframe = document.getElementById('gameFrame');
-    const playButton = document.getElementById('playButton');
+function togglePlay(modalId) {
+    const iframe = document.getElementById(`gameFrame${modalId}`);
+    const playButton = document.getElementById(`playButton${modalId}`);
     if (playButton.innerText === 'Start Game') {
         playButton.innerText = 'Stop Game';
         iframe.contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*'); // For iframe videos
@@ -20,10 +40,10 @@ document.getElementById('playButton').addEventListener('click', function() {
         playButton.innerText = 'Start Game';
         iframe.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*'); // For iframe videos
     }
-});
+}
 
-function toggleFullscreen() {
-    const modal = document.getElementById('gameModal');
+function toggleFullscreen(modalId) {
+    const modal = document.getElementById(`gameModal${modalId}`);
     if (modal.requestFullscreen) {
         modal.requestFullscreen();
     } else if (modal.mozRequestFullScreen) { /* Firefox */
@@ -36,12 +56,18 @@ function toggleFullscreen() {
 }
 
 document.addEventListener('fullscreenchange', function() {
-    const modal = document.getElementById('gameModal');
-    if (document.fullscreenElement === modal) {
-        document.querySelector('.modal-content').style.display = 'none';
-        document.querySelector('.fullscreen-button').style.display = 'none';
+    const fullscreenElement = document.fullscreenElement;
+    if (fullscreenElement && fullscreenElement.classList.contains('modal')) {
+        fullscreenElement.querySelector('.modal-content').style.display = 'none';
+        fullscreenElement.querySelector('.fullscreen-button').style.display = 'none';
+        fullscreenElement.querySelector('.close-button').style.display = 'none';
+        fullscreenElement.querySelector('.play-button').style.display = 'none';
     } else {
-        document.querySelector('.modal-content').style.display = 'block';
-        document.querySelector('.fullscreen-button').style.display = 'block';
+        document.querySelectorAll('.modal').forEach(modal => {
+            modal.querySelector('.modal-content').style.display = 'block';
+            modal.querySelector('.fullscreen-button').style.display = 'block';
+            modal.querySelector('.close-button').style.display = 'block';
+            modal.querySelector('.play-button').style.display = 'block';
+        });
     }
 });
