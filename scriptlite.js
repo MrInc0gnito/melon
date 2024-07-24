79f7fec9cea4decaabaf62439aede29710e06a7d
MrIncognitoAKAnitro <contact.itzscratchy@gmail.com>
2024-07-24 14:39:33 +0100

    Update scriptlite.js

--- scriptlite.js
+++ scriptlite.js
@@ -1,28 +1,37 @@
-function openGameModal(gameTitle) {
-    document.getElementById("gameTitle").innerText = gameTitle;
-    document.getElementById("gameModal").style.display = "block";
+// JavaScript to handle modal and game controls
+function openGameModal(gameName) {
+    const modal = document.getElementById('gameModal');
+    document.getElementById('gameTitle').innerText = gameName;
+    document.getElementById('gameFrame').src = ''; // Set URL later
+    modal.style.display = 'block';
 }
 
 function closeGameModal() {
-    document.getElementById("gameModal").style.display = "none";
+    const modal = document.getElementById('gameModal');
+    modal.style.display = 'none';
 }
+
+document.getElementById('playButton').addEventListener('click', function() {
+    const iframe = document.getElementById('gameFrame');
+    const playButton = document.getElementById('playButton');
+    if (playButton.innerText === 'Start Game') {
+        playButton.innerText = 'Stop Game';
+        iframe.contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*'); // For iframe videos
+    } else {
+        playButton.innerText = 'Start Game';
+        iframe.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*'); // For iframe videos
+    }
+});
 
 function toggleFullscreen() {
-    var iframe = document.getElementById("gameFrame");
-    if (iframe.requestFullscreen) {
-        iframe.requestFullscreen();
-    } else if (iframe.mozRequestFullScreen) { /* Firefox */
-        iframe.mozRequestFullScreen();
-    } else if (iframe.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
-        iframe.webkitRequestFullscreen();
-    } else if (iframe.msRequestFullscreen) { /* IE/Edge */
-        iframe.msRequestFullscreen();
-    }
-}
-
-window.onclick = function(event) {
-    var modal = document.getElementById("gameModal");
-    if (event.target == modal) {
-        modal.style.display = "none";
+    const modal = document.getElementById('gameModal');
+    if (modal.requestFullscreen) {
+        modal.requestFullscreen();
+    } else if (modal.mozRequestFullScreen) { /* Firefox */
+        modal.mozRequestFullScreen();
+    } else if (modal.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+        modal.webkitRequestFullscreen();
+    } else if (modal.msRequestFullscreen) { /* IE/Edge */
+        modal.msRequestFullscreen();
     }
 }
