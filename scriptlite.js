8b1029c03f262f5f912ca6520ee08f8e9af9d372
MrIncognitoAKAnitro <contact.itzscratchy@gmail.com>
2024-07-24 10:35:04 +0100

    Update scriptlite.js

--- scriptlite.js
+++ scriptlite.js
@@ -1,1 +1,28 @@
+function openGameModal(gameTitle) {
+    document.getElementById("gameTitle").innerText = gameTitle;
+    document.getElementById("gameModal").style.display = "block";
+}
 
+function closeGameModal() {
+    document.getElementById("gameModal").style.display = "none";
+}
+
+function toggleFullscreen() {
+    var iframe = document.getElementById("gameFrame");
+    if (iframe.requestFullscreen) {
+        iframe.requestFullscreen();
+    } else if (iframe.mozRequestFullScreen) { /* Firefox */
+        iframe.mozRequestFullScreen();
+    } else if (iframe.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+        iframe.webkitRequestFullscreen();
+    } else if (iframe.msRequestFullscreen) { /* IE/Edge */
+        iframe.msRequestFullscreen();
+    }
+}
+
+window.onclick = function(event) {
+    var modal = document.getElementById("gameModal");
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
