fa8ed67325f5413d77e9af8753555ebe5719227f
MrIncognitoAKAnitro <contact.itzscratchy@gmail.com>
2024-08-06 14:54:16 +0100

    Update scriptlite.js

--- scriptlite.js
+++ scriptlite.js
@@ -1,4 +1,3 @@
-// Create modals for each game
 const games = [
     { id: 1, name: "Miniblox", url: "https://miniblox.io/" },
     { id: 2, name: "Voxiom", url: "https://voxiom.io/" },
@@ -13,13 +12,15 @@ games.forEach(game => {
             <div class="modal-content">
                 <span class="close-button" onclick="closeGameModal(${game.id})">&times;</span>
                 <h2 id="gameTitle${game.id}">${game.name}</h2>
-                <iframe id="gameFrame${game.id}" src="${game.url}" width="100%" height="400px" frameborder="0" allowfullscreen></iframe>
-                <button id="playButton${game.id}" class="play-button" onclick="togglePlay(${game.id})">Start Game</button>
-                <button class="fullscreen-button" onclick="toggleFullscreen(${game.id})">
-                    <svg class="fullscreen-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                        <path d="M32 32C14.3 32 0 46.3 0 64l0 96c0 17.7 14.3 32 32 32s32-14.3 32-32l0-64 64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L32 32zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 96c0 17.7 14.3 32 32 32l96 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0 0-64zM320 32c-17.7 0-32 14.3-32 32s14.3 32 32 32l64 0 0 64c0 17.7 14.3 32 32 32s32-14.3 32-32l0-96c0-17.7-14.3-32-32-32l-96 0zM448 352c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64-64 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l96 0c17.7 0 32-14.3 32-32l0-96z"/>
-                    </svg>
-                </button>
+                <iframe id="gameFrame${game.id}" data-src="${game.url}" width="100%" height="400px" frameborder="0" allowfullscreen></iframe>
+                <div class="modal-controls">
+                    <button id="playButton${game.id}" class="play-button" onclick="togglePlay(${game.id})">Start Game</button>
+                    <button class="fullscreen-button" onclick="toggleFullscreen(${game.id})">
+                        <svg class="fullscreen-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                            <path d="M32 32C14.3 32 0 46.3 0 64l0 96c0 17.7 14.3 32 32 32s32-14.3 32-32l0-64 64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L32 32zM64 352c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 96c0 17.7 14.3 32 32 32l96 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0 0-64zM320 32c-17.7 0-32 14.3-32 32s14.3 32 32 32l64 0 0 64c0 17.7 14.3 32 32 32s32-14.3 32-32l0-96c0-17.7-14.3-32-32-32l-96 0zM448 352c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 64-64 0c-17.7 0-32 14.3-32 32s14.3 32 32 32l96 0c17.7 0 32-14.3 32-32l0-96z"/>
+                        </svg>
+                    </button>
+                </div>
             </div>
         </div>
     `;
@@ -35,18 +36,25 @@ function openGameModal(gameName, modalId) {
 function closeGameModal(modalId) {
     const modal = document.getElementById(`gameModal${modalId}`);
     modal.style.display = 'none';
+    stopGame(modalId); // Stop the game and cut off music when the modal is closed
 }
 
 function togglePlay(modalId) {
     const iframe = document.getElementById(`gameFrame${modalId}`);
     const playButton = document.getElementById(`playButton${modalId}`);
+    
     if (playButton.innerText === 'Start Game') {
         playButton.innerText = 'Stop Game';
-        iframe.contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*'); // For iframe videos
+        iframe.src = iframe.getAttribute('data-src'); // Load the game URL
     } else {
         playButton.innerText = 'Start Game';
-        iframe.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*'); // For iframe videos
+        stopGame(modalId); // Stop the game
     }
+}
+
+function stopGame(modalId) {
+    const iframe = document.getElementById(`gameFrame${modalId}`);
+    iframe.src = ''; // Stop any ongoing media by clearing the src
 }
 
 function toggleFullscreen(modalId) {
@@ -63,22 +71,9 @@ function toggleFullscreen(modalId) {
 }
 
 document.addEventListener('fullscreenchange', function() {
-    const fullscreenElement = document.fullscreenElement;
-    if (fullscreenElement && fullscreenElement.classList.contains('modal')) {
-        fullscreenElement.querySelector('.modal-content').style.display = 'none';
-        fullscreenElement.querySelector('.fullscreen-button').style.display = 'none';
-        fullscreenElement.querySelector('.close-button').style.display = 'none';
-        fullscreenElement.querySelector('.play-button').style.display = 'none';
-        fullscreenElement.querySelector('iframe').style.width = '100vw';
-        fullscreenElement.querySelector('iframe').style.height = '100vh';
-    } else {
-        document.querySelectorAll('.modal').forEach(modal => {
-            modal.querySelector('.modal-content').style.display = 'block';
-            modal.querySelector('.fullscreen-button').style.display = 'block';
-            modal.querySelector('.close-button').style.display = 'block';
-            modal.querySelector('.play-button').style.display = 'block';
-            modal.querySelector('iframe').style.width = '100%';
-            modal.querySelector('iframe').style.height = '400px';
-        });
+    const fullscreenElement = document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement || document.msFullscreenElement;
+    if (!fullscreenElement) {
+        document.querySelectorAll('.modal').forEach(modal => modal.style.display = 'none');
+        document.querySelectorAll('.play-button').forEach(button => button.innerText = 'Start Game');
     }
 });
